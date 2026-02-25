73c1f438a57f5d72cd6da5b36e000bbd140eb84f
Lebron224 <zachlegros25@gmail.com>
2026-02-25 16:44:09 -0500

    Entrain de setup la base du code

--- Backend/backend/wsgi.py
+++ Backend/backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simulation_boursiere project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simulation_boursiere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
