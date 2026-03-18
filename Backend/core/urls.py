c0a95be22953de6c6ca7d1723351ef71e56734f2
Lebron224 <zachlegros25@gmail.com>
2026-03-18 15:59:32 -0400

    continuation

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StockViewSet, PortfolioViewSet
+ 
+router = DefaultRouter()
+router.register(r'stocks', StockViewSet)
+router.register(r'portfolio', PortfolioViewSet)
  
 urlpatterns = [
-    path('portfolio/', get_portfolio),
-    path('buy/', buy_stock),
+    path('api/', include(router.urls)),
 ]
