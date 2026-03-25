64ea1a23a2ac73f48aec6bdeef683485ef2a08d2
Brunokt123 <2491777@bdeb.qc.ca>
2026-03-25 13:47:35 -0400

    download pc
    django
    vc

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -41,6 +41,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
