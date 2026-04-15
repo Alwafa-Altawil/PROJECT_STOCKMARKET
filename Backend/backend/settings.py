af3364a61661ec9fa782b91e820d7ec375aae8bc
brunokwanteau <2491777@bdeb.qc.ca>
2026-04-15 14:18:23 -0400

    commencer les commentaires backend

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -14,27 +14,27 @@ SECRET_KEY = 'django-insecure-$&e&is-8!p8pv4_bg55p8sn)%f7ublzfit1uhdn@s+951g4l=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] #vide , local
 
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ #applications de django installées dans le projet qui peuvent etre utilisées pour nous aider 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'api.apps.ApiConfig',
-    'rest_framework',
+ # applications autres que django 
+    'api.apps.ApiConfig', 
+    'rest_framework', 
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'core.apps.CoreConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [ # composantes qui prennent les requests pour des changements spécifiques
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -45,19 +45,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [ #origines autorisées pour le frontend
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
 ]
 
-# Useful if you later use session/cookie auth from frontend.
+
 CORS_ALLOW_CREDENTIALS = True
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'backend.urls' #fichier principal des urls
 
-TEMPLATES = [
+TEMPLATES = [ # templates utilisés pour les pages web
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
@@ -72,16 +72,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application' #wsgi pour les applications web 
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { #configuration de la base de données
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', #database sqlite3 
         'NAME': BASE_DIR / 'db.sqlite3',
+
+
     }
 }
 
@@ -89,18 +91,18 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [ #règles à suivre pour la création d'un mot de passe pour un utilisateur
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', #vérifie si mot de passe est similaire à l'utilisateur
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', #vérifie si mot de passe est au moins 8 caractères
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', #vérifie si mot de passe est courant
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', #vérifie si mot de passe est numérique
     },
 ]
 
@@ -108,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us' #langue par défaut
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC' #temps par défaut
 
-USE_I18N = True
+USE_I18N = True 
 
-USE_TZ = True
+USE_TZ = True 
 
 
 # Static files (CSS, JavaScript, Images)
@@ -122,18 +124,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-REST_FRAMEWORK = {
+REST_FRAMEWORK = { #configuration de la gestion des permissions
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication', #authentification par token
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated', 
     ),
 }
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+SIMPLE_JWT = { #configuration des tokens
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15), #temps de validité du token
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7), #temps de validité du token de rafraîchissement
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path , include
  
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/core/', include('core.urls')),
+    path('admin/', admin.site.urls), #admin interface pour la gestion des données
+    path('api/core/', include('core.urls')), #api interface pour la gestion des données
 ]
